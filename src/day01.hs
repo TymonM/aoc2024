5e3f20b291917a44a874090613c528def96074d4
tymon <tymon.mieszkowski@gmail.com>
2024-12-11 19:45:58 +1300

    init and day01

---
+++ src/day01.hs
@@ -0,0 +1,34 @@
+import System.IO
+import Data.List (sort)
+
+first :: IO ()
+second :: IO ()
+
+main :: IO ()
+main = do
+    first
+    second
+
+parseInput :: IO ([Int], [Int])
+parseInput = do
+    input <- readFile "day01.txt"
+    let res = unzip (map ((\[x, y] -> (read x :: Int, read y :: Int)) . words) (lines input))
+    return res
+
+first = do
+    (lista, listb) <- parseInput
+
+    let sorteda = sort lista
+    let sortedb = sort listb
+
+    -- let diff = sum $ map (\(a,b)->abs (a-b)) (zip sorteda sortedb)
+    let diff = sum $ zipWith (\ a b -> abs (a-b) ) sorteda sortedb
+
+    print diff
+
+second = do
+    (lista, listb) <- parseInput
+
+    let total = sum $ map (\ x -> x * length (filter (==x) listb)) lista
+
+    print total
